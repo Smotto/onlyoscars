35e7a72f05bb2cc5fd909d956359ec1d4bc5c5c5
Jarod Castillo <Jarodc2017@gmail.com>
2021-03-27 06:25:03 -0700

    search by category, not case sensitive.

--- routes/category.js
+++ routes/category.js
@@ -9,6 +9,27 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Displaying Categories', body: "category data should be in the body?"});
 });
 
+/* GET request */
+router.get('/:category', function(req, res, next) {
+    console.log("GET Request Received for: Specific Page.");
+    let mineNow = req.params.category.toUpperCase();
+    console.log(mineNow);
+    function addFindings()
+    {
+        let movieDataList = [];
+        for(let element in req.app.locals.moviedata)
+        {
+            if(req.app.locals.moviedata[element].category === mineNow)
+            {
+                movieDataList.push(JSON.stringify(req.app.locals.moviedata[element]));
+            }
+        }
+        return movieDataList;
+    }
+
+    res.render('specificPage', { title: 'Specific Page', body: addFindings()});
+});
+
 module.exports = router;
 
 
