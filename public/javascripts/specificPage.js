71945907a59e49512d2eba6bbbd1076731351861
amadshah209 <amadshah@csus.edu>
2021-05-14 17:39:30 -0700

    listing and home button (not formatted)

--- public/javascripts/specificPage.js
+++ public/javascripts/specificPage.js
@@ -1,27 +1,101 @@
 console.log("Specific Page F12 Console.")
 
 let movieData = document.getElementById('movieDataID');
-if(movieData.innerText < 3) {
-    window.alert("No Results, redirecting to homepage");
-    window.history.back();
-}
-else {
-    let parsedData = JSON.parse(movieData.innerText);
+let parsedData = JSON.parse(movieData.innerText);
+
+//for (let element in parsedData)
+//{
+//console.log(parsedData[element]);
+//}
+
+let entity = [];
+
+function getEntity() {
     for (let element in parsedData) {
-        console.log(parsedData[element]);
+        entity.push(parsedData[element].entity)
     }
+
+   // console.log(entity);
+
+
+    let string = "";
+    for (let index = 0; index < entity.length; index++) {
+        string += " " + entity[index] + "\n";
+    }
+
+    document.getElementById('entity').innerText = string;
 }
 
-// TODO: Filter out the data
-// reGEX },
-// ^_^ Line above separates the movie objects visually, not literally though.
-// Further filter out the data {",:
-// Replace the pug variable with your new filtered data :)
+getEntity();
 
-// Step 1: Target the data
-// Step 2: Do stuff with the data
-// Step 3: Replace the data
-// Step 4: Profit
+let movieYear = [];
 
-//TODO: Make the div in style.css nice.
-// You can probably make it a nice box for each, or something.
+function getYear() {
+    for (let element in parsedData) {
+        movieYear.push(parsedData[element].year)
+    }
+
+     console.log(movieYear);
+
+     let string = "";
+     for (let index = 0; index < movieYear.length; index++) {
+          string +=  movieYear[index] + "\n";
+      }
+     document.getElementById('year').innerText = string;
+}
+
+    getYear();
+
+    let winner = [];
+
+    function getWinner() {
+        for (let element in parsedData) {
+            winner.push(parsedData[element].winner)
+        }
+
+        // console.log(winner);
+
+        let string = "";
+        for (let index = 0; index < movieYear.length; index++) {
+            string +=  winner[index] + "\n";
+
+        }
+
+
+        document.getElementById('winner').innerText = string;
+    }
+
+    getWinner();
+
+    let tempCategory = [];
+
+    function getCategory() {
+        for (let element in parsedData) {
+            tempCategory.push(parsedData[element].category)
+        }
+        let category = [...new Set(tempCategory)]
+
+        console.log(category);
+    }
+
+    getCategory();
+
+
+
+//let movieDataJSONParsed = JSON.parse(document.getElementById('movieDataID').innerText);
+//console.log(movieDataJSONParsed);
+
+//function displayData() {
+//    for (let i in movieDataJSONParsed) {
+//        let row =`<tr>
+//            <td>${movieDataJSONParsed[i].entity}</td>
+//           <td>${movieDataJSONParsed[i].movieYear}</td>
+//            <td>${movieDataJSONParsed[i].winner}</td>
+//         </tr>`
+
+//        let table = $('#table-body')
+//       table.append(row);
+
+//    }
+//}
+//displayData();
